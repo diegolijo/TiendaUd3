32f5ccddb089a4d8d32b85921c7f6f53a6700580
diegolijo <diegolijo@gmail.com>
2020-01-23 15:46:47 +0100

    a ver si e desta..

--- app/src/main/java/com/jito/tiendaud3/dummy/datos/BaseDatos.java
+++ app/src/main/java/com/jito/tiendaud3/dummy/datos/BaseDatos.java
@@ -3,7 +3,7 @@ package com.jito.tiendaud3.dummy.datos;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Usuario.class, Pedido.class}, version = 1)
+@Database(entities = {Usuario.class, Pedido.class}, version = 1,exportSchema = false)
 public abstract class BaseDatos extends RoomDatabase {
     public abstract DeviceDao Dao();
 }
